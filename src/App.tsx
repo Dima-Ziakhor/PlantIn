43c61eda70e8aa769ef7595ceab1def687484665
Dmitriy Ziakhor <dmziakhor@gmail.com>
2022-01-14 02:02:08 +0200

    Init progect

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello world!</h1>
+      <h1 className="title">Init progect</h1>
     </div>
   );
 }
